9c9fa39197a222ad013b76b9c5af4387b694ce2c
ZAFIR123456 <102862385+ZAFIR123456@users.noreply.github.com>
2023-07-23 13:32:15 +0500

    Initial commit

---
+++ datatypes.js
@@ -0,0 +1,63 @@
+// Data types in computer programming are used to categorize and represent different types of data
+// that can be used in a program. Each data type defines the size, format, and operations that can be
+//  performed on the data. In JavaScript, there are several built-in data types:
+
+
+// ******************************
+// 1. PRIMITIVE DATA TYPES
+// ******************************
+
+
+
+// * Number
+//  Represents both integer and floating-point numbers. For example, 42 or 3.14.
+    
+    let number = 42;
+
+// * String
+// Represents a sequence of characters. Enclosed in single ('') or double quotes (""). 
+// For example, 'Hello' or "JavaScript".
+
+    let String = "Hello world"
+
+// * Boolean
+// Represents a logical value true or false.
+
+    let Boolean = true
+
+// * Null
+//  Represents an intentional absence of any value.
+
+    let empty = null
+
+// * Undefined
+// Represents a declared variable that hasn't been assigned a value.
+
+    let notDefined;  
+
+
+// ******************************
+// 2. NON-PRIMITIVE DATA TYPES
+// ******************************
+
+
+
+// * Array
+// Represents a collection of elements in a specific order. For example, [1, 2, 3, 4].
+
+    let num = [1, 2, 3, 4, 5, 6]
+
+// * Objects
+// Represents a collection of key-value pairs. For example, { name: 'John', age: 25 }.
+
+    let person = {
+        name:"zafir",
+        age:20
+    }
+
+// * Functons
+// Represents a reusable block of code that performs a specific task.
+
+    let hello = function(){
+        console.log("hello")
+    }
---
+++ variables.js
@@ -0,0 +1,33 @@
+// Variables are an essential concept in programming, including JavaScript.
+// They are used to store and represent data values that can change during the program's execution.
+// Each variable has a name (identifier) and a value associated with it.
+
+// In JavaScript, you can declare variables using the var, let, or const keywords:
+
+// For example
+
+// using var is the older way to declare a variable
+
+var name = "ali";
+var age = 28;
+
+// using var you can also update value of 'name'
+
+var name = "ahmed";
+
+// console.log(name, age)
+// using let is the new way to declare a variable
+
+let score = 100;
+let playerName = "Alice";
+
+// using let you can also update value of 'score'
+
+score = 200;
+// console.log(score, playerName)
+
+// using const is for constant
+
+const pi = 3.14;
+
+// pi = 3.14159; using const you cannot change the value of pi it will givean error
