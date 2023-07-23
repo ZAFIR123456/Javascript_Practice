b5287c3a393675fbf969bfe6c290e622bc49f9a7
ZAFIR123456 <102862385+ZAFIR123456@users.noreply.github.com>
2023-07-23 13:41:57 +0500

    operators topic done

---
+++ operators.js
@@ -0,0 +1,57 @@
+// Operators in programming are symbols or keywords that perform specific operations on one or more 
+// operands (values or variables). In JavaScript, operators can be classified into several categories
+//  based on their functionality.
+
+
+{"TYPES OF OPERATOR"}
+
+
+{"Arithmetic Operators:"}
+
+// +: Addition
+// -: Subtraction
+// *: Multiplication
+// /: Division
+// %: Modulo (Remainder after division)
+
+{"Assignment Operators:"}
+
+// =: Simple assignment
+// +=: Add and assign
+// -=: Subtract and assign
+// *=: Multiply and assign
+// /=: Divide and assign
+// %=: Modulo and assign
+
+{"Comparison Operators:"}
+
+// ==: Equal to (loose equality, type coercion)
+// ===: Equal to (strict equality, no type coercion)
+// !=: Not equal to (loose inequality, type coercion)
+// !==: Not equal to (strict inequality, no type coercion)
+// >: Greater than
+// <: Less than
+// >=: Greater than or equal to
+// <=: Less than or equal to
+
+{"Logical Operators:"}
+
+// &&: Logical AND
+// ||: Logical OR
+// !: Logical NOT
+
+{"Unary Operators:"}
+
+// ++: Increment by 1
+// --: Decrement by 1
+// -: Unary negation (converts a value to its negative)
+
+{"Conditional (Ternary) Operator:"}
+
+// condition ? expr1 : expr2: If the condition is true, expr1 is evaluated; otherwise, expr2 is evaluated.
+
+
+{"Type Operators:"}
+
+// typeof: Returns a string representing the data type of a value.
+// instanceof: Checks if an object is an instance of a specific class.
