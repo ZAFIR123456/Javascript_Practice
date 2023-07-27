477f332231fdfd6df432efcca3aa5e2857b2cad6
ZAFIR123456 <102862385+ZAFIR123456@users.noreply.github.com>
2023-07-27 11:36:50 +0500

    spred and rest operators

--- operators.js
+++ operators.js
@@ -55,3 +55,65 @@
 
 // typeof: Returns a string representing the data type of a value.
 // instanceof: Checks if an object is an instance of a specific class.
+
+
+
+{"Spread & rest operators"}
+
+// The spread and rest operators are two important features introduced in ECMAScript 6 (ES6) that make working 
+// with arrays and function arguments more convenient and powerful.
+
+
+{"Spread Operator (...):"}
+
+// The spread operator is denoted by three dots (...). It is used to split an array or object into individual 
+// elements or properties.
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+// Combining two arrays using spread operator
+const combinedArray = [...arr1, ...arr2]; // [1, 2, 3, 4, 5, 6]
+
+
+// The spread operator can also be used to create shallow copies of arrays and objects:
+
+
+const originalArray = [1, 2, 3];
+const copyArray = [...originalArray]; // Creates a copy of originalArray
+
+const originalObject = { a: 1, b: 2 };
+const copyObject = { ...originalObject }; // Creates a copy of originalObject
+
+
+{"Rest Operator (...):"}
+
+// The rest operator also uses the three dots (...) syntax. It is used in function parameters to gather all the 
+// remaining arguments into an array. It allows functions to accept a variable number of arguments.
+
+
+// Using rest parameter to accept multiple arguments
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+  }
+  
+  const result = sum(1, 2, 3, 4, 5); // 15
+
+
+// The rest operator can also be used to handle a part of the arguments while passing the rest to another 
+// function:
+
+
+
+function multiply(multiplier, ...numbers) {
+    return numbers.map((num) => num * multiplier);
+  }
+  
+  const multipliedNumbers = multiply(2, 1, 2, 3, 4); // [2, 4, 6, 8]
+
+  
+// In this example, the first argument 2 is assigned to the multiplier parameter, and the rest of the 
+// arguments 1, 2, 3, 4 are gathered into the numbers array.
+
+// Both the spread and rest operators are powerful tools that enhance the flexibility and readability of 
+// JavaScript code, especially when working with arrays and function arguments.
